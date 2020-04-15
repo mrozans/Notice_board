bfc86182bad83705a4dacf38de89dff2a1354a02
tomasz <tjozwik@protonmail.com>
2020-04-15 18:12:59 +0200

    IPv6 and IPv4 server (for testing purposes only!)

--- Test/server.rb
+++ Test/server.rb
@@ -1,16 +1,12 @@
 require 'socket'
+require 'thread'
 
 include Socket::Constants
 
-socket = Socket.new(AF_INET, SOCK_STREAM, 0)
-sockaddr = Socket.sockaddr_in(2001, '0.0.0.0')
-socket.bind(sockaddr)
-socket.listen(5)
-
-loop do
+def handle_connection(socket, service_type)
   client, client_addrinfo = socket.accept
-
-  puts "- - - - - #{Time.now}", client.inspect, client_addrinfo.inspect
+  
+  puts "-> #{service_type}: #{Time.now}", client.inspect, client_addrinfo.inspect
 
   begin
     pair = client.recvfrom_nonblock(1024)
@@ -22,4 +18,35 @@ loop do
   puts pair.first.inspect
 end
 
-socket.close
+threads = []
+
+# IPv6 server
+
+socket_ipv6 = Socket.new(AF_INET6, SOCK_STREAM, 0)
+sockaddr_ipv6 = Socket.sockaddr_in(2000, '*')
+socket_ipv6.bind(sockaddr_ipv6)
+socket_ipv6.listen(5)
+
+threads << Thread.new do
+  loop do
+    handle_connection(socket_ipv6, 'ipv6_thread')
+  end
+end
+
+# IPv4 server
+
+socket_ipv4 = Socket.new(AF_INET, SOCK_STREAM, 0)
+sockaddr_ipv4 = Socket.sockaddr_in(2000, '0.0.0.0')
+socket_ipv4.bind(sockaddr_ipv4)
+socket_ipv4.listen(5)
+
+threads << Thread.new do
+  loop do
+    handle_connection(socket_ipv4, 'ipv4_thread')
+  end
+end
+
+threads.each { |t| t.join }
+
+socket_ipv6.close
+socket_ipv4.close
